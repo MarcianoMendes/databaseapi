3352de2c5fa1415640feff3f7e6727d5836ec318
marcianomendes <nmendes94@hotmail.com>
2022-03-21 22:28:18 -0300

    add find and update to user

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,6 +1,21 @@
+const user = require("../models/User");
 var User = require("../models/User");
 class UserController {
-    async index(req, res) { }
+    async index(req, res) {
+        res.json(await User.allUsers());
+    }
+
+    async findUser(req, res) {
+        var id = req.params.id;
+        var user = await User.findById(id);
+        if (user == undefined) {
+            res.status(404);
+            res.json({});
+            return;
+        }
+
+        res.json(user);
+    }
 
     async create(req, res) {
         var { name, email, password } = req.body;
@@ -15,7 +30,7 @@ class UserController {
             res.json({ message: "O email é inválido" })
             return;
         }
-        
+
         if (await User.findEmail(email)) {
             res.status(406);
             res.json({ message: "O email já existe na base de dados" })
@@ -31,6 +46,24 @@ class UserController {
         await User.create(name, email, password);
         res.status(200);
         res.send("Usuário inserido com sucesso!");
+    }
+
+    async edit(req, res){
+        var { id, name, email, role} = req.body;
+        var result = await User.update(id, name, email, role);
+        if(result != undefined){
+            if(result.status){
+                res.send("Usuário atualizado com sucesso!");
+                return;
+            }
+
+            res.status(406);
+            res.send(result.message);
+            return;
+        }
+       
+        res.status(406);
+        res.send("Ocorreo um erro no processo de edição");
     }
 }
 
--- models/User.js
+++ models/User.js
@@ -2,11 +2,35 @@
 var knex = require("../database/connection");
 var bcrypt = require("bcrypt");
 class User {
+    async allUsers() {
+        try {
+            return await knex.select("id", "name", "email", "role").table("users");
+
+        } catch (err) {
+            console.log(err);
+            return [];
+        }
+    }
+
+    async findById(id) {
+        try {
+            var result = await knex.select("id", "name", "email", "role").where({ id: id }).table("users");
+            if (result.length > 0) {
+                return result[0];
+            }
+
+            return undefined;
+
+        } catch (err) {
+            console.log(err);
+            return undefined;
+        }
+    }
+
     async create(name, email, password) {
         try {
             var hash = await bcrypt.hash(password, 10);
             await knex.insert({ name, email, password: hash, role: 0 }).table("users");
-
         } catch (err) {
             console.log(err)
         }
@@ -15,12 +39,46 @@ class User {
     async findEmail(email) {
         try {
             var result = await knex.select("email").from("users").where({ email: email });
-            console.log(result);
             return result.length > 0;
         } catch (err) {
             console.log(err);
             return false;
         }
+    }
+
+    async update(id, name, email, role) {
+        var user = await this.findById(id);
+        if (user != undefined) {
+            var editUser = {};
+            if (email != undefined) {
+                if (email != user.email) {
+                    var result = await this.findEmail(email);
+                    if (result != undefined) {
+                        return { status: false, message: "O e-mail já está cadastrado!" }
+                    }
+
+                    editUser.email = email;
+                }
+            }
+
+            if (name != undefined) {
+                editUser.name = name;
+            }
+
+            if (role != undefined) {
+                editUser.role = role;
+            }
+
+            try {
+                await knex.update(editUser).where({ id: id }).table("users");
+            } catch (err) {
+                return { status: false, message: err };
+            }
+
+            return { status: true };
+        }
+
+        return { status: false, message: "Usuário não existe!" }
     }
 }
 
